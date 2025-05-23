e4d388ce5875fa9d6e247a642af5d12ad074c378
albertosdz <albert.santosdiaz8@gmail.com>
2025-05-23 12:00:36 +0200

    TFG Añadiendo la vista de la sección de proyectos

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -14,8 +14,14 @@ class DashboardController
 
         isAuth();
 
+        $id = $_SESSION['id'];
+
+        $proyectos = Proyecto::belongsTo('usuarioId', $id);
+
+
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
@@ -55,6 +61,26 @@ class DashboardController
             'alertas' => $alertas
         ]);
     }
+
+    public static function proyecto(Router $router) {
+
+        session_start();
+        isAuth();
+        $alertas = [];
+
+        $token = $_GET['id'];
+        if(!$token) header('Location: /dashboard');
+        // Aseguramos los proyectos para que sean privados
+        $proyecto = Proyecto::where('url', $token);
+        if ($proyecto->usuarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
+        ]);
+    }
+
     public static function perfil(Router $router)
     {
         session_start();
