16a902cf99628d6f5db8a1077e813c53b6a6668b
albertosdz <albert.santosdiaz8@gmail.com>
2025-05-28 14:26:25 +0200

    TFG Añadiendo Tareas a los Proyectos

---
+++ controllers/TareaController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Controllers;
+
+use Model\Proyecto;
+use Model\Tarea;
+
+class TareaController
+{
+    public static function index() {}
+    public static function crear()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            session_start();
+
+            $proyectoId = $_POST['proyectoId'];
+
+            $proyecto = Proyecto::where('url', $proyectoId);
+
+            if (!$proyecto || $proyecto->usuarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'hubo un Error al agregar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+           
+            // No hay errores, instanciamos y creamos la tarea
+            $tarea = new Tarea($_POST);
+            $tarea->proyectoId = $proyecto->id;
+            $resultado = $tarea->guardar();
+            $respuesta = [
+                'tipo' => 'exito',
+                'id' => $resultado['id'],
+                'mensaje' => 'Tarea Creada Correctamente'
+            ];
+            echo json_encode($respuesta);
+        }
+    }
+    public static function actualizar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+    public static function eliminar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+}
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -31,6 +31,7 @@
     }, 0);
 
     modal.addEventListener("click", function (e) {
+      e.preventDefault();
       if (e.target.classList.contains("cerrar-modal")) {
         const formulario = document.querySelector(".formulario");
         formulario.classList.add("cerrar");
@@ -39,8 +40,89 @@
           modal.remove();
         }, 500);
       }
+      if (e.target.classList.contains("submit-nueva-tarea")) {
+        submitFormularioNuevaTarea();
+      }
     });
 
-    document.querySelector("body").appendChild(modal);
+    document.querySelector(".dashboard").appendChild(modal);
+  }
+
+  function submitFormularioNuevaTarea() {
+    const tarea = document.querySelector("#tarea").value.trim();
+
+    if (tarea === "") {
+      // Mostramos alerta de error
+      mostrarAlerta(
+        "El nombre de la tarea es obligatorio",
+        "error",
+        document.querySelector(".formulario legend")
+      );
+      return;
+    }
+
+    agregarTarea(tarea);
+  }
+
+  function mostrarAlerta(mensaje, tipo, referencia) {
+    //prevenir la creacion de multiples alertas
+    const alertaPrevia = document.querySelector(".alerta");
+    if (alertaPrevia) {
+      alertaPrevia.remove();
+    }
+
+    const alerta = document.createElement("DIV");
+    alerta.classList.add("alerta", tipo);
+    alerta.textContent = mensaje;
+
+    // Insertar alerta antes del legend
+    referencia.parentElement.insertBefore(
+      alerta,
+      referencia.nextElementSibling
+    );
+
+    // Eliminar la alerta después de 5 segundos
+    setTimeout(() => {
+      alerta.remove();
+    }, 5000);
+  }
+
+  // Consultamos el Servidor para añadir una nueva tarea al proyecto actual
+  async function agregarTarea(tarea) {
+    // Construir la petición
+    const datos = new FormData();
+    datos.append("nombre", tarea);
+    datos.append("proyectoId", obtenerProyecto());
+
+    try {
+      const url = "http://localhost:3000/api/tarea";
+      const respuesta = await fetch(url, {
+        method: "POST",
+        body: datos,
+      });
+
+      const resultado = await respuesta.json();
+
+      mostrarAlerta(
+        resultado.mensaje,
+        resultado.tipo,
+        document.querySelector(".formulario legend")
+      );
+
+      if (resultado.tipo === "exito") {
+        const modal = document.querySelector(".modal");
+        setTimeout(() => {
+          modal.remove();
+        }, 3000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function obtenerProyecto() {
+    const proyectoParams = new URLSearchParams(window.location.search);
+    const proyecto = Object.fromEntries(proyectoParams.entries());
+    return proyecto.id;
   }
 })();
