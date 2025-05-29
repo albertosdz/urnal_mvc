dff70e96df5960455d777b5fb00d6cc905557a71
albertosdz <albert.santosdiaz8@gmail.com>
2025-05-29 18:25:09 +0200

    TFG Cambiando el estado de las tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -59,7 +59,33 @@ class TareaController
     public static function actualizar()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            # code...
+            // Validamos que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['proyectoId']);
+
+            session_start();
+
+            if (!$proyecto || $proyecto->usuarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'hubo un Error al actualizar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+
+            $tarea = new Tarea($_POST);
+            $tarea->proyectoId = $proyecto->id;
+
+            $resultado = $tarea->guardar();
+            if ($resultado) {
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'id' => $tarea->id,
+                    'proyectoId' => $proyecto->id
+                ];
+                echo json_encode(['respuesta' => $respuesta]);
+            }
+
         }
     }
     public static function eliminar()
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -55,6 +55,9 @@
       btnEstadoTarea.classList.add(`${estados[tarea.estado].toLowerCase()}`);
       btnEstadoTarea.textContent = estados[tarea.estado];
       btnEstadoTarea.dataset.estadoTarea = tarea.estado;
+      btnEstadoTarea.onclick = function () {
+        cambiarEstadoTarea({ ...tarea });
+      };
 
       const btnEliminarTarea = document.createElement("BUTTON");
       btnEliminarTarea.classList.add("eliminar-tarea");
@@ -193,6 +196,45 @@
         };
 
         tareas = [...tareas, tareaObj];
+        mostrarTareas();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function cambiarEstadoTarea(tarea) {
+    const nuevoEstado = tarea.estado === "1" ? "0" : "1";
+    tarea.estado = nuevoEstado;
+    actualizarTarea(tarea);
+  }
+
+  async function actualizarTarea(tarea) {
+    const { estado, id, nombre, proyectoId } = tarea;
+
+    const datos = new FormData();
+    datos.append("id", id);
+    datos.append("nombre", nombre);
+    datos.append("estado", estado);
+    datos.append("proyectoId", obtenerProyecto());
+
+    try {
+      const url = "http://localhost:3000/api/tarea/actualizar";
+
+      const respuesta = await fetch(url, {
+        method: "POST",
+        body: datos,
+      });
+      const resultado = await respuesta.json();
+
+      if (resultado.respuesta.tipo === "exito") {
+        tareas = tareas.map((tareaMemoria) => {
+          if (tareaMemoria.id === id) {
+            tareaMemoria.estado = estado;
+          }
+          return tareaMemoria;
+        });
+
         mostrarTareas();
       }
     } catch (error) {
